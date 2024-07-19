56f7c9dcdfbf5b990bdd04b87e59a4822bde00cb
Blackburn0 <hamzatadebayo5@gmail.com>
2024-07-19 10:58:32 -0400

    module 4 updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -27,3 +28,4 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+        
